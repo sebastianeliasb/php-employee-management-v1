0d23e30a3a2ec562006156727d20317266596271
Sebastian Brupbacher <sebastian.eliasb@gmail.com>
2021-07-05 16:10:22 +0200

    style: Navbar creation

--- index.php
+++ index.php
@@ -12,14 +12,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./node_modules/bootstrap/dist/css/bootstrap-reboot.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <script src="./node_modules/jquery/dist/jquery.min.js"></script>
-    <link rel="stylesheet" href="assets\css\login.css">
+    <script src="./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="./node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" href="assets/css/login.css">
     <title>Document</title>
 </head>
 
 <body>
+  
     <div class="wrapper fadeInDown">
         <div id="formContent">
             <!-- Tabs Titles -->
--- src/dashboard.php
+++ src/dashboard.php
@@ -1,1 +1,21 @@
 <!-- TODO Main view or Employees Grid View here is where you get when logged here there's the grid of employees -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="../node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="../node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+    <?php
+  require_once("../assets/html/header.html");
+  ?>
+    </header>
+    
+</body>
+</html>

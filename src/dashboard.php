45f6fe9c3ff6b464e4de08876f117041b26ca1e9
imalovelyghost <imalovelyghost13@gmail.com>
2021-07-05 17:19:29 +0200

    feat/style Dashboard

--- src/dashboard.php
+++ src/dashboard.php
@@ -1,6 +1,7 @@
 <!-- TODO Main view or Employees Grid View here is where you get when logged here there's the grid of employees -->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,14 +9,58 @@
     <script src="../node_modules/jquery/dist/jquery.min.js"></script>
     <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <link href="../node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" href="../assets/css/main.css">
+
     <title>Document</title>
 </head>
+
 <body>
     <header>
-    <?php
-  require_once("../assets/html/header.html");
-  ?>
-    </header>
-    
-</body>
-</html>
+        <?php
+        require_once("../assets/html/header.html");
+        //Table title init
+
+        echo "<h4>Dashboard Employee</h4>
+        <div class='table-wrapper'>
+            <table class='fl-table'>
+                <thead>
+                    <tr>
+                        <th>NAME</th>
+                        <th>LAST NAME</th>
+                        <th>AGE</th>
+                        <th>EMAIL</th>
+                        <th>PHONE NUMBER</th>
+                    </tr>
+                </thead>
+            <tbody>";
+
+        //Table title end
+
+        $filepath = '../resources/employees.json';
+        $json_string = file_get_contents($filepath);
+        $json = json_decode($json_string, true);
+
+
+        array_walk(
+            $json,
+            function ($employee_data) {
+                // print_r($employee_data);
+                $name =  $employee_data["name"]; // Access Array data
+                $lastName = $employee_data["lastName"];
+                $age = $employee_data["age"];
+                $email =  $employee_data["email"];
+                $phoneNumber = $employee_data["phoneNumber"];
+
+                echo "<tr>";
+                echo " <td>" . $name . "</td>";
+                echo " <td>" . $lastName . "</td>";
+                echo " <td>" . $age . "</td>";
+                echo " <td>" . $email . "</td>";
+                echo " <td>" . $phoneNumber . "</td>";
+                echo "</tr>";
+            }
+        );
+        echo   "<tbody>
+            </table>
+            </div>";
+        ?>

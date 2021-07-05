16413cf3cee064e05c038e559a9007395e3f5e39
Jordi <aejordi@gmail.com>
2021-07-05 15:17:22 +0200

    feat: login user check done

--- src/library/loginController.php
+++ src/library/loginController.php
@@ -1,22 +1,12 @@
 <?php
 session_start();
 
-$email = $_POST["email"];
-$pass = $_POST["pass"];
+include './loginManager.php';
 
-
-
-$emailDb = "sebastian.eliasb@gmail.com";
-$passDb = "12345";
-
-$passDbHash = password_hash($passDb,PASSWORD_DEFAULT);
-
-if(password_verify($pass,$passDbHash) && $email == $emailDb) {
-    $_SESSION["email"] = $email;
-    header("Location: ../welcome.php");
+if (isset($_POST["email"]) && isset($_POST["pass"])) {
+    login($userData);
 }
-else {
+/* else {
     $_SESSION["loginError"] = "Wrong email or password";
     header("Location: ../errorpage.php");
-}
-
+} */

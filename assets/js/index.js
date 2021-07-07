287eb65d62b63db525515275840c49e184a3cf6f
Sebastian Brupbacher <sebastian.eliasb@gmail.com>
2021-07-07 18:10:45 +0200

    feat: second append function

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,10 +2,23 @@ const infoRow = $(".info-row");
 const addNewEmployee = $(".new-employee");
 const employeeInfo = $(".employee-info");
 
-$(addNewEmployee).on("click", function () {
-  infoRow.append(
+// $(addNewEmployee).on("click", function () {
+//   infoRow.append(
+//     "<tr><td><input type='text' name='name' id='name'></td><td><input type='text' name='lastName' id='lastName'></td><td><input type='number' name='age' id='age'></td><td><input type='text' name='email' id='email'></td><td><input type='number' name='phoneNumber' id='phoneNumber'></td><td><button type='submit' name='new_submit' >+</tr>"
+//   );
+// });
+
+addNewEmployee.on("click", addNewEmployeeFunc);
+
+function addNewEmployeeFunc() {
+  $(".info-row").append(
     "<tr><td><input type='text' name='name' id='name'></td><td><input type='text' name='lastName' id='lastName'></td><td><input type='number' name='age' id='age'></td><td><input type='text' name='email' id='email'></td><td><input type='number' name='phoneNumber' id='phoneNumber'></td><td><button type='submit' name='new_submit' >+</tr>"
   );
+  // $(addNewEmployee).off("click", $(".info-row"), addNewEmployeeFunc);
+}
+
+infoRow.on("load", function () {
+  addNewEmployee.on("click", addNewEmployeeFunc);
 });
 
 $("#new-employee-form").submit(function (e) {

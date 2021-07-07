1169b025d64dbae2c97577d06e682bfbf014b45a
Jordi <aejordi@gmail.com>
2021-07-07 15:24:47 +0200

    feat: refactores and created submit form function

--- src/library/employeeController.php
+++ src/library/employeeController.php
@@ -5,23 +5,17 @@ require_once './employeeManager.php';
 $data = json_decode(file_get_contents('../../resources/employees.json'), true);
 
 if ($data != null && isset($_POST['submitEmployee'])) {
-    $newData =
-        [
-            'id' => $_GET['id'],
-            'name' => $_POST['name'],
-            'lastName' => $_POST['lastName'],
-            'email' => $_POST['email'],
-            'gender' => $_POST['gender'],
-            'city' => $_POST['city'],
-            'streetAddress' => $_POST['streetAddress'],
-            'state' => $_POST['state'],
-            'age' => $_POST['age'],
-            'postalCode' => $_POST['zip'],
-            'phoneNumber' => $_POST['phone'],
-        ];
 
-    updateEmployee($newData);
+    $newData = getFormData($data);
+    updateEmployee($newData, $data);
 }
+
+if ($data != null && isset($_POST['newEmployee'])) {
+
+    $newData = getFormData($data);
+    addEmployee($newData, $data);
+}
+
 
 
 

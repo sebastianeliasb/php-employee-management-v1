dcfda03a4e6306766c090ab250e37eac0e013521
Jordi <aejordi@gmail.com>
2021-07-08 09:30:23 +0200

    fix: event listeners and load problem solved

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,26 +2,13 @@ const infoRow = $(".info-row");
 const addNewEmployee = $(".new-employee");
 const employeeInfo = $(".employee-info");
 
-// $(addNewEmployee).on("click", function () {
-//   infoRow.append(
-//     "<tr><td><input type='text' name='name' id='name'></td><td><input type='text' name='lastName' id='lastName'></td><td><input type='number' name='age' id='age'></td><td><input type='text' name='email' id='email'></td><td><input type='number' name='phoneNumber' id='phoneNumber'></td><td><button type='submit' name='new_submit' >+</tr>"
-//   );
-// });
-
-addNewEmployee.on("click", addNewEmployeeFunc);
-
-function addNewEmployeeFunc() {
+$("body").on("click", ".new-employee", function () {
   $(".info-row").append(
     "<tr><td><input type='text' name='name' id='name'></td><td><input type='text' name='lastName' id='lastName'></td><td><input type='number' name='age' id='age'></td><td><input type='text' name='email' id='email'></td><td><input type='number' name='phoneNumber' id='phoneNumber'></td><td><button type='submit' name='new_submit' >+</tr>"
   );
-  // $(addNewEmployee).off("click", $(".info-row"), addNewEmployeeFunc);
-}
-
-infoRow.on("load", function () {
-  addNewEmployee.on("click", addNewEmployeeFunc);
 });
 
-$("#new-employee-form").submit(function (e) {
+$("body").on("submit", "#new-employee-form", function (e) {
   const postData = {
     id: $("tr").length - 1,
     name: $("#name").val(),
@@ -40,28 +27,24 @@ $("#new-employee-form").submit(function (e) {
     "../src/library/employeeController.php",
     postData,
     function (response) {
-      console.log(response);
+      console.log("response");
     }
   );
   e.preventDefault();
-  infoRow.load("../src/dashboard.php .info-row");
+  let employeesRequest = $.ajax({
+    type: "GET",
+    url: "./dashboard.php",
+  });
+
+  employeesRequest.done(function (data, status) {
+    $("tbody").html($(data).find("tbody").html());
+  });
 });
-$("td").on("click", function (e) {
+
+$("body").on("click", ".toForm", function (e) {
   location.replace("../src/employee.php?id=" + $(e.target).data("id"));
 });
 
 $("#returnEmployees").on("click", () => {
   location.replace("../src/dashboard.php");
 });
-
-/* $(function () {
-  let employeesRequest = $.ajax({
-    type: "GET",
-    url: "../resources/employees.json",
-  });
-
-  employeesRequest.done(function (data, status) {
-    console.log(data);
-    console.log(status);
-  });
-}); */
